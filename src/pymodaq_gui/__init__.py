ba89f39e4be72229d6f0a668a1930faec7f8fc93
Sébastien Weber <seba.weber@gmail.com>
2025-02-10 11:47:40 +0100

    Update __init__.py

--- src/pymodaq_gui/__init__.py
+++ src/pymodaq_gui/__init__.py
@@ -35,59 +35,47 @@ try:
         __version__ = '0.0.0dev'
     try:
         logger = set_logger('pymodaq_gui', base_logger=False)
+        logger.info('')
+        logger.info('')
+        logger.info('****************************')
+        logger.info('Starting PyMoDAQ GUI modules')
+        logger.info('****************************')
+        logger.info('')
+        logger.info('')
+
+        # in a try statement for compilation on readthedocs server but if this fail, you cannot use the code
+        from pymodaq_gui.plotting import data_viewers  # imported here as to avoid circular imports later on
+        from pymodaq_gui.qt_utils import setLocale, set_qt_backend
+
+        from pymodaq_utils.config import Config
+        from pymodaq_data.plotting.plotter.plotter import register_plotter, PlotterFactory
+
+        config = Config()  # to ckeck for config file existence, otherwise create one
+
+        logger.info('************************')
+        logger.info(f"Setting Qt backend to: {config['qtbackend']['backend']} ...")
+        set_qt_backend()
+        logger.info('************************')
+        logger.info('')
+        logger.info('')
+        logger.info('************************')
+        logger.info(f"Setting Locale to {config['style']['language']} / {config['style']['country']}")
+        logger.info('************************')
+        setLocale()
+        logger.info('')
+        logger.info('')
+
+        logger.info('')
+        logger.info('')
+        logger.info('************************')
+        logger.info(f"Registering PyMoDAQ qt plotters...")
+        register_plotter(parent_module_name='pymodaq_gui.plotting.plotter')
+        logger.info(f"Done")
+        logger.info('************************')
+
     except Exception:
         print("Couldn't create the local folder to store logs , presets...")
 
-    logger.info('')
-    logger.info('')
-    logger.info('************************')
-    logger.info('Starting PyMoDAQ modules')
-    logger.info('************************')
-    logger.info('')
-    logger.info('')
-
-
-    # in a try statement for compilation on readthedocs server but if this fail, you cannot use the code
-    from pymodaq_gui.plotting import data_viewers  # imported here as to avoid circular imports later on
-    from pymodaq_gui.qt_utils import setLocale, set_qt_backend
-
-    from pymodaq_utils.config import Config
-    from pymodaq_data.plotting.plotter.plotter import register_plotter, PlotterFactory
-
-    # issue on windows when using .NET code within multithreads, this below allows it but requires the
-    # pywin32 (pythoncom) package
-    if importlib.util.find_spec('clr') is not None:
-        try:
-            import pythoncom
-            pythoncom.CoInitialize()
-        except ModuleNotFoundError as e:
-            infos = "You have installed plugins requiring the pywin32 package to work correctly," \
-                    " please type in *pip install pywin32* and restart PyMoDAQ"
-            print(infos)
-            logger.warning(infos)
-
-    config = Config()  # to ckeck for config file existence, otherwise create one
-
-    logger.info('************************')
-    logger.info(f"Setting Qt backend to: {config['qtbackend']['backend']} ...")
-    set_qt_backend()
-    logger.info('************************')
-    logger.info('')
-    logger.info('')
-    logger.info('************************')
-    logger.info(f"Setting Locale to {config['style']['language']} / {config['style']['country']}")
-    logger.info('************************')
-    setLocale()
-    logger.info('')
-    logger.info('')
-
-    logger.info('')
-    logger.info('')
-    logger.info('************************')
-    logger.info(f"Registering PyMoDAQ qt plotters...")
-    register_plotter(parent_module_name='pymodaq_gui.plotting.plotter')
-    logger.info(f"Done")
-    logger.info('************************')
 
 except Exception as e:
     try:
