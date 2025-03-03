acfb0d31c91353eb3ca18dfe6cd3251f2596e94c
Sébastien Weber <seba.weber@gmail.com>
2025-03-03 16:07:42 +0100

    adding a spinbox accepting key sequence as shortcuts

--- src/pymodaq_gui/utils/__init__.py
+++ src/pymodaq_gui/utils/__init__.py
@@ -4,7 +4,7 @@ from .file_io import select_file
 from .widgets.table import TableView, TableModel, SpinBoxDelegate, BooleanDelegate
 from .widgets.push import PushButtonIcon, EditPush, EditPushRel, EditPushInfo
 from .widgets.qled import QLED
-from .widgets.spinbox import QSpinBox_ro
+from .widgets.spinbox import QSpinBox_ro, QSpinBoxWithShortcut
 from .custom_app import CustomApp
 
 from .utils import set_dark_palette
