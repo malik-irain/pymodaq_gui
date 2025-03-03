9720ab31c2ae1db8af1ee6289725bdc7d2434c99
Sébastien Weber <seba.weber@gmail.com>
2025-03-03 17:37:08 +0100

    set working default shortcuts

--- src/pymodaq_gui/utils/widgets/spinbox.py
+++ src/pymodaq_gui/utils/widgets/spinbox.py
@@ -20,14 +20,18 @@ class QSpinBoxWithShortcut(SpinBox):
     """
     QSpinBox but which accepts key sequences and store them as attribute
     For now, it does not apply to regular input such as text or numerics.
+
+    Beware I could not make it run for the KeySequence Ctrl+Enter or any combination involving enter...
+
     """
-    def __init__(self, *args, key_sequences=("Ctrl+Enter",), **kwargs):
+    def __init__(self, *args, key_sequences=("Ctrl+E",), **kwargs):
+
         super().__init__(*args, **kwargs)
 
         self.shortcut = dict() #Store shortcuts in a dictionnary
         for key_sequence in key_sequences:
             shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(key_sequence), self)
-            shortcut.setContext(QtCore.Qt.ShortcutContext.WidgetWithChildrenShortcut)
+            shortcut.setContext(QtCore.Qt.WidgetWithChildrenShortcut)
             self.shortcut[key_sequence] = shortcut
 
 
@@ -39,3 +43,19 @@ class QSpinBox_ro(SpinBox):
         self.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
 
 
+if __name__ == '__main__':
+
+    from pymodaq_gui.utils.utils import mkQApp
+
+    app = mkQApp('Test Spinbox')
+
+    spinbox = QSpinBoxWithShortcut(key_sequences=('Ctrl+E', 'Ctrl+Shift+E', 'Ctrl+Enter', 'Ctrl+Shift+Enter'))
+    def print_spinbox(value):
+        print(value)
+    spinbox.shortcut['Ctrl+E'].activated.connect(lambda: print_spinbox(f'Ctrl+E: {spinbox.value()}'))
+    spinbox.shortcut['Ctrl+Shift+E'].activated.connect(lambda: print_spinbox(f'Ctrl+Shift+E: {spinbox.value()}'))
+    spinbox.shortcut['Ctrl+Shift+Enter'].activated.connect(lambda: print_spinbox(f'Ctrl+Shift+Enter: {spinbox.value()}'))
+    spinbox.shortcut['Ctrl+Enter'].activatedAmbiguously.connect(lambda: print_spinbox(f'Ctrl+Enter: {spinbox.value()}'))
+    spinbox.show()
+
+    app.exec()
