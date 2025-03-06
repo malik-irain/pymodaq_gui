f699c95016c02a1f6873a2fe5ad7f3823e795b40
Sébastien Weber <seba.weber@gmail.com>
2025-03-06 10:38:38 +0100

    wrong default spinbox shortcut

--- tests/utils/widgets/spinbox_test.py
+++ tests/utils/widgets/spinbox_test.py
@@ -8,6 +8,6 @@ def test_spinbox_shortcut(qtbot):
     spinbox = QSpinBoxWithShortcut()
     def print_spinbox(value):
         print(value)
-    spinbox.shortcut['Ctrl+Enter'].activated.connect(lambda: print_spinbox(spinbox.value()))
+    spinbox.shortcut['Ctrl+E'].activated.connect(lambda: print_spinbox(spinbox.value()))
     spinbox.show()
 
