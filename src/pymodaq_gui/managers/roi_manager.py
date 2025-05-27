5f09035993062ef6c5f9da6ae00363c37f1db6d7
Malik Irain <malik.irain@cemes.fr>
2025-05-27 11:33:32 +0200

    qtpy backends check and selection works correctly
    Renamed config.py into config_saver_loader.py to avoid name conflicts
    Replaced check_qt_backend and set_qt_backend with a unique function

--- src/pymodaq_gui/managers/roi_manager.py
+++ src/pymodaq_gui/managers/roi_manager.py
@@ -21,7 +21,7 @@ from pyqtgraph import functions as fn
 from pyqtgraph import LinearRegionItem as pgLinearROI
 from pymodaq_utils.utils import plot_colors
 from pymodaq_utils.logger import get_module_name, set_logger
-from pymodaq_gui.config import get_set_roi_path
+from pymodaq_gui.config_saver_loader import get_set_roi_path
 from pymodaq_gui.utils import select_file
 from pymodaq_gui.plotting.utils import plot_utils
 
