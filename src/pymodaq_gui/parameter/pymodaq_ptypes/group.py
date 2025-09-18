f522e2b1b439c5cd2f3608df31b976e5b4345c9e
Ashwola <schouderconstant@outlook.com>
2025-09-18 18:15:24 +0200

    Adding a new file for GroupParameterItem/GroupParameter with addMenu keyword

---
+++ src/pymodaq_gui/parameter/pymodaq_ptypes/group.py
@@ -0,0 +1,209 @@
+from qtpy import QtWidgets, QtCore
+from pyqtgraph.parametertree.parameterTypes.basetypes import ParameterItem, Parameter
+
+
+class GroupParameterItem(ParameterItem):
+    """
+    Group parameters are used mainly as a generic parent item that holds (and groups!) a set
+    of child parameters. It also provides a simple mechanism for displaying a button or combo
+    that can be used to add new parameters to the group.
+    """
+
+    def __init__(self, param, depth):
+        ParameterItem.__init__(self, param, depth)
+        self._initialFontPointSize = self.font(0).pointSize()
+        self.updateDepth(depth)
+
+        self.addItem = None
+        if 'addText' in param.opts:
+            self.addText(param)
+
+        self.optsChanged(self.param, self.param.opts)
+    
+    def addText(self, param):
+        addText = param.opts['addText']
+        if 'addList' in param.opts:
+            self.addWidget = QtWidgets.QComboBox()
+            self.addWidget.setSizeAdjustPolicy(QtWidgets.QComboBox.SizeAdjustPolicy.AdjustToContents)
+            self.updateAddList()
+            self.addWidget.currentIndexChanged.connect(self.addChanged)
+        elif "addMenu" in param.opts:
+            # Create a QPushButton that will show the menu
+            self.addWidget = QtWidgets.QPushButton(addText)
+            # Create the nested menu
+            self.addMenu = QtWidgets.QMenu(self.addWidget)
+            self.addWidget.setMenu(self.addMenu)
+            # Populate the nested menu structure
+            self.updateAddMenu()                  
+        else:
+            self.addWidget = QtWidgets.QPushButton(addText)
+            self.addWidget.clicked.connect(self.addClicked)
+        w = QtWidgets.QWidget()
+        l = QtWidgets.QHBoxLayout()
+        l.setContentsMargins(0, 0, 0, 0)
+        w.setLayout(l)
+        l.addWidget(self.addWidget)
+        l.addStretch()
+        self.addWidgetBox = w
+        self.addItem = QtWidgets.QTreeWidgetItem([])
+        self.addItem.setFlags(QtCore.Qt.ItemFlag.ItemIsEnabled)
+        self.addItem.depth = self.depth + 1
+        ParameterItem.addChild(self, self.addItem)
+        self.addItem.setSizeHint(0, self.addWidgetBox.sizeHint())        
+        
+    def pointSize(self):
+        return self._initialFontPointSize
+
+    def updateDepth(self, depth):
+        """
+        Change set the item font to bold and increase the font size on outermost groups.
+        """
+        for c in [0, 1]:
+            font = self.font(c)
+            font.setBold(True)
+            if depth == 0:
+                font.setPointSize(self.pointSize() + 1)
+            self.setFont(c, font)
+        self.titleChanged()  # sets the size hint for column 0 which is based on the new font
+
+    def addClicked(self):
+        """Called when "add new" button is clicked
+        The parameter MUST have an 'addNew' method defined.
+        """
+        self.param.addNew()
+
+    def addChanged(self):
+        """Called when "add new" combo is changed
+        The parameter MUST have an 'addNew' method defined.
+        """
+        if self.addWidget.currentIndex() == 0:
+            return
+        typ = self.addWidget.currentText()
+        self.param.addNew(typ)
+        self.addWidget.setCurrentIndex(0)
+
+    def treeWidgetChanged(self):
+        ParameterItem.treeWidgetChanged(self)
+        tw = self.treeWidget()
+        if tw is None:
+            return
+        self.setFirstColumnSpanned(True)
+        if self.addItem is not None:
+            tw.setItemWidget(self.addItem, 0, self.addWidgetBox)
+            self.addItem.setFirstColumnSpanned(True)
+
+    def addChild(self, child):  ## make sure added childs are actually inserted before add btn
+        if self.addItem is not None:
+            ParameterItem.insertChild(self, self.childCount() - 1, child)
+        else:
+            ParameterItem.addChild(self, child)
+
+    def optsChanged(self, param, opts):
+        ParameterItem.optsChanged(self, param, opts)
+
+        if 'addList' in opts:
+            self.updateAddList()
+
+        if 'addMenu' in opts:
+            self.updateAddMenu()            
+
+        if hasattr(self, 'addWidget'):
+            if 'enabled' in opts:
+                self.addWidget.setEnabled(opts['enabled'])
+
+            if 'tip' in opts:
+                self.addWidget.setToolTip(opts['tip'])
+
+    def updateAddList(self):
+        self.addWidget.blockSignals(True)
+        try:
+            self.addWidget.clear()
+            self.addWidget.addItem(self.param.opts['addText'])
+            for t in self.param.opts['addList']:
+                self.addWidget.addItem(t)
+        finally:
+            self.addWidget.blockSignals(False)
+
+
+    def updateAddMenu(self):
+        self.addWidget.blockSignals(True)
+        try:
+            self.addMenu.clear()
+            addMenu = self.param.opts.get('addMenu', [])
+            self._buildMenuFromIterable(self.addMenu, addMenu)
+        finally:
+            self.addWidget.blockSignals(False)
+
+    def _buildMenuFromIterable(self, menu, items, path=()):
+        if isinstance(items, dict):
+            for key, value in items.items():
+                self._handleMenuItem(menu, key, value, path)
+        elif isinstance(items, (list, tuple)):
+            for item in items:
+                if isinstance(item, dict):
+                    for key, value in item.items():
+                        self._handleMenuItem(menu, key, value, path)
+                elif isinstance(item, str):
+                    self._addLeafAction(menu, item, path + (item,))
+
+    def _handleMenuItem(self, menu: QtWidgets.QMenu, key, value, path):
+        """Handle a single menu item (key-value pair)"""
+        new_path = path + (key,)
+
+        if self._isNested(value):
+            # Create submenu and recurse
+            submenu = menu.addMenu(key)
+            self._buildMenuFromIterable(submenu, value, new_path)
+        else:
+            # Create leaf action
+            self._addLeafAction(menu, key, new_path)
+
+    def _isNested(self, value):
+        """Check if a value represents nested structure"""
+        return isinstance(value, (dict, list, tuple)) and value  # Not empty
+
+    def _addLeafAction(self, menu: QtWidgets.QMenu, name, path):
+        """Add a leaf action to the menu"""
+        action = menu.addAction(name)
+        action.triggered.connect(lambda checked, data=path: self.addMenuItemSelected(data))
+
+
+
+    def addMenuItemSelected(self, path_tuple):
+        """Called when a menu item is selected from the nested add menu
+        The parameter MUST have an 'addNew' method defined.
+        """
+        # Call the parameter's addNew method with the selected type
+        self.param.addNew(path_tuple)
+
+        # Reset the button text back to the original addText
+        # (equivalent to setCurrentIndex(0) for the combo)
+        if hasattr(self.param.opts, 'addText'):
+            self.addWidget.setText(self.param.opts['addText'])
+
+class GroupParameter(Parameter):
+    """
+    Group parameters are used mainly as a generic parent item that holds (and groups!) a set
+    of child parameters.
+
+    It also provides a simple mechanism for displaying a button or combo
+    that can be used to add new parameters to the group. To enable this, the group
+    must be initialized with the 'addText' option (the text will be displayed on
+    a button which, when clicked, will cause addNew() to be called). If the 'addList'
+    option is specified as well, then a dropdown-list of addable items will be displayed
+    instead of a button.
+    """
+    itemClass = GroupParameterItem
+
+    sigAddNew = QtCore.Signal(object, object)  # self, type
+
+    def addNew(self, typ=None):
+        """
+        This method is called when the user has requested to add a new item to the group.
+        By default, it emits ``sigAddNew(self, typ)``.
+        """
+        self.sigAddNew.emit(self, typ)
+
+    def setAddList(self, vals):
+        """Change the list of options available for the user to add to the group."""
+        self.setOpts(addList=vals)            
