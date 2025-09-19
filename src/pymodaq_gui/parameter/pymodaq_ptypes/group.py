bcb1b0616e939764e6189f8b030742654ecac66d
Ashwola <schouderconstant@outlook.com>
2025-09-19 10:46:21 +0200

    using inheritance for groupparameter

--- src/pymodaq_gui/parameter/pymodaq_ptypes/group.py
+++ src/pymodaq_gui/parameter/pymodaq_ptypes/group.py
@@ -1,5 +1,5 @@
 from qtpy import QtWidgets, QtCore
-from pyqtgraph.parametertree.parameterTypes.basetypes import ParameterItem, Parameter
+from pyqtgraph.parametertree.parameterTypes.basetypes import ParameterItem, GroupParameter
 
 
 class GroupParameterItem(ParameterItem):
@@ -181,29 +181,10 @@ class GroupParameterItem(ParameterItem):
         if hasattr(self.param.opts, 'addText'):
             self.addWidget.setText(self.param.opts['addText'])
 
-class GroupParameter(Parameter):
-    """
-    Group parameters are used mainly as a generic parent item that holds (and groups!) a set
-    of child parameters.
-
-    It also provides a simple mechanism for displaying a button or combo
-    that can be used to add new parameters to the group. To enable this, the group
-    must be initialized with the 'addText' option (the text will be displayed on
-    a button which, when clicked, will cause addNew() to be called). If the 'addList'
-    option is specified as well, then a dropdown-list of addable items will be displayed
-    instead of a button.
-    """
+class GroupParameter(GroupParameter):
+    
     itemClass = GroupParameterItem
 
-    sigAddNew = QtCore.Signal(object, object)  # self, type
-
-    def addNew(self, typ=None):
-        """
-        This method is called when the user has requested to add a new item to the group.
-        By default, it emits ``sigAddNew(self, typ)``.
-        """
-        self.sigAddNew.emit(self, typ)
-
-    def setAddList(self, vals):
-        """Change the list of options available for the user to add to the group."""
-        self.setOpts(addList=vals)            
+    def __init__(self, **opts):
+        super().__init__(**opts)
+   
